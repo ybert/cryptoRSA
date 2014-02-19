b0a0a5e0a0d2b642b654596b807276c6b982c042
Cedric Chabert <ced.chabert@gmail.com>
2014-02-19 16:58:51 +0100

    pgcd creation

--- src/tpcrypto2/Main.java
+++ src/tpcrypto2/Main.java
@@ -5,9 +5,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-
+			
 	}
         
         de la merde
 
+	
+	public int pgcd(int m, int n) {
+		   while ( n != 0 ) {
+		     //int r = m mod n;
+			 int r =1;
+		     m = n;
+		     n = r;
+		   }
+		   return m;
+		 }
 }
